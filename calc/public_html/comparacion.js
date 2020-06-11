62ae8ce35127a41ec4ab7dea762b166b5aac9b59
Luciifer <Luciifer@Luciifer2>
2020-06-11 17:27:24 -0500

    botones concatenados en curso

--- calc/public_html/comparacion.js
+++ calc/public_html/comparacion.js
@@ -3,5 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+function uno(){
+    var un=document.getElementById("resultado").value;
+    if(un=="0"){
+        document.getElementById("resultado").value="1";
+    }else {
+        var u="1";
+        var rsu=un.concat(u);
+        document.getElementById("resultado").value=rsu;
+    }
+}
