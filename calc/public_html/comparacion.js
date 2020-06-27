6163a60804b189a6e9ab79a1ab83753751be8660
Luciifer <Luciifer@Luciifer2>
2020-06-26 19:45:46 -0500

    y los comentarios

--- calc/public_html/comparacion.js
+++ calc/public_html/comparacion.js
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//añadiendo pruebas
 function uno(){
     var un=document.getElementById("resultado").value;
     if(un=="0"){
